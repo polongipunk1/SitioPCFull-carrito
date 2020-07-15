fdb5beacc16ca78895db55dd1c4fc5294df819fd
Jerry Anthony Gabriel Jacuinde <bandoler71@gmail.com>
2020-07-15 16:33:46 -0500

    prueba

---
+++ SitioPCFull/Models/CarritoItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitioPCFull.Models
+{
+    public class CarritoItem
+    {
+        private productos _producto;
+
+        public productos Producto
+        {
+            get { return _producto; }
+            set { _producto = value; }
+        }
+        private int _cantidad;
+
+        public int Cantidad
+        {
+            get { return _cantidad; }
+            set { _cantidad = value; }
+        }
+        public CarritoItem()
+        {
+
+        }
+        public CarritoItem(productos producto, int cantidad)
+        {
+            this._producto = producto;
+            this._cantidad = cantidad;
+        }
+    }
+}
